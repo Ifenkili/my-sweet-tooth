0e8744888d44a4097dc76f076e4cc69d7424de24
Ifenkili <47796852+Ifenkili@users.noreply.github.com>
2020-01-06 01:32:25 +0200

    12 files uploaded
    Index HTML, CSS, JS and 6 asset files uploaded

---
+++ mySweetTooth.js
@@ -0,0 +1,15 @@
+//Send a welcome message when page loads
+
+function loadAlert() {
+  alert("Welcome! O sweet toothed one :)");
+
+}
+//Change the size of the writing in the paragraph
+
+function fontSize() {
+  document.getElementById ('changeSize').style.fontSize = 'xx-large';
+}
+//send a message when a form is submitted
+function submitAlert() {
+  alert('Thanks for your message! I will get back to you as soon as possible.');
+  }
